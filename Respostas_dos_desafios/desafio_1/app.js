2487c6e746afcef050cc436b1f532ebae4ea325a
Alexferraz2 <alex_ferraz2@hotmail.com>
2022-07-03 15:47:54 -0300

    Inclusão de desafios e respostas do desafio de Tyescrit da DIO

---
+++ Respostas_dos_desafios/desafio_1/app.js
@@ -0,0 +1,31 @@
+"use strict";
+//Minha resposta
+;
+const sandro = {
+    code: 11,
+    name: 'Sandro',
+    profession: 'Developer'
+};
+const alex = {
+    code: 10,
+    name: 'Alex',
+};
+//Resopsta do professor
+// Resposta 1
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+// Resposta 2
+const funcionario2 = {
+    codigo: 10,
+    nome: 'joao'
+};
+;
+const funcionarioObj = {};
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+const funcionarioObj2 = {
+    codigo: 10,
+    nome: 'João'
+};
---
+++ Respostas_dos_desafios/desafio_1/app.ts
@@ -0,0 +1,48 @@
+//Minha resposta
+
+interface Employee {
+    code: Number,
+    name: String,
+    profession?: String,
+
+};
+
+const sandro: Employee = {
+    code: 11,
+    name: 'Sandro',
+    profession: 'Developer'
+}
+
+const alex: Employee = {
+    code: 10,
+    name: 'Alex',
+}
+
+//Resopsta do professor
+
+// Resposta 1
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
+// Resposta 2
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+}
+
+// Respostas 3 e 4
+interface Funcionario {  // Já conhece interfaces? https://blog.logrocket.com/types-vs-interfaces-in-typescript/
+    codigo: number,
+    nome: string
+};
+
+const funcionarioObj = {} as Funcionario;
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+
+const funcionarioObj2: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+}
